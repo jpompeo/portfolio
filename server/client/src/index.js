3ddd592057d90ee06229b15a62a1c520a9085069
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-08-06 21:30:05 -0400

    Setting up index and file tree

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment } from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from './components/App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
+import reducers from "./reducers";
+import "bootstrap/dist/css/bootstrap.css";
+import './styles/index.css'
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Fragment>
+        <App/>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
