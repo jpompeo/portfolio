22c9800e455d6e4e8dc031379faafa2f81a11dd6
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-09-04 18:54:24 -0400

    Adding link and description for Write Track

--- server/client/src/components/Demo.js
+++ server/client/src/components/Demo.js
@@ -4,8 +4,11 @@ import '../styles/Demo.css';
 
 const Demo = () => {
     return (
-        <div id="demo-video">
-
+        <div id="demo-writetrack">
+            <h1>Capstone Project: <a href="http://write-track.herokuapp.com">Write Track</a></h1>
+            <p><strong>Technologies:</strong> React, Redux, React-Bootstrap, Node.js, Express, MongoDB, Mongoose, Chart.js</p>
+            <p><strong>Description:</strong>Developed for authors in the process of writing a book, this app assists with setting goals, tracking progress, and planning out steps. Writing a book is a long process, and it’s easy to lose momentum along the way. Write Track is designed to keep writers motivated with the visualization of progress and celebration of achievements every step of the way.</p>
+            <h2>Demo:</h2>
             <YouTube
                 video="zuL34MyogEw"
                 startSeconds={(60 * 37) + 17}
@@ -14,7 +17,6 @@ const Demo = () => {
                 height={315}
                 allowFullscreen
             />
-
         </div>
     )
 }
