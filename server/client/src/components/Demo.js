d9048050fb83bc9a18cf38c910a493bc1bbf5570
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-09-04 17:55:52 -0400

    Creating component for demo

---
+++ server/client/src/components/Demo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Youtube from 'react-youtube';
+
+const Demo = () => {
+    return (
+        <h1>demo</h1>
+    )
+}
+
+export Demo;
--- server/client/src/components/Main.js
+++ server/client/src/components/Main.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import '../styles/Main.css';
 import Bio from './Bio';
+import Demo from './Demo';
 
 const Main = () => {
     return (
         <Container fluid>
             <Row id="main-page">
                 <Col lg={5}>
+                    {/* Headshot and Bio */}
                     <Bio/>
                 </Col>
                 <Col lg={7}>
-                    <div>
-                        Demo
-                    </div>
+                    {/* Final project demo video */}
+                    <Demo/>
+
                     <div>
                         Links
                     </div>
