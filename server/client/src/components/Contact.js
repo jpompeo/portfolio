adcc1b315946eb82472b1bfbbc5f970cd6eb277f
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-09-14 17:18:42 -0400

    Adding links and info to contact page

--- server/client/src/components/Contact.js
+++ server/client/src/components/Contact.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Row, Col, Container } from "react-bootstrap";
+import '../styles/Contact.css';
 
 const Contact = () => {
     return (
-        <div id="contact"> 
-        <h1>Contact Me</h1>
-        </div>
+        <Container id="contact" fluid>
+            <Row>
+                <Col>
+                    {/* <Link to="/home">
+                        <Button className="back-button">&larr; menu</Button>
+                    </Link> */}
+                    <header id="contact-title">
+                        <h1>Contact Me</h1>
+                    </header>
+                </Col>
+            </Row>
+
+            <Row>
+                {/* <Col sm={6} md={5} lg={5}>
+                     Headshot 
+                    <div id="headshot-container">
+                        <img id="contact-image" src="" alt="" />
+                    </div>
+                </Col> */}
+
+                <Col>
+                    {/* Bio */}
+                        <div id="contact-list-container">
+                            <ul id="contact-list">
+                            <li>
+                                <img src="" className="contact-icon" alt="Email icon"/>
+                                <p className="contact-header">Email</p>
+                                <p className="contact-info">
+                                    <a href="mailto:joannapompeo@gmail.com">joannapompeo@gmail.com</a>
+                                </p>
+                            </li>
+                            <li>
+                            <img src="" className="contact-icon" alt="Phone icon"/>
+                                <p className="contact-header">Phone</p>
+                                <p className="contact-info">
+                                    <a href="tel:561-236-1615">561-236-1615</a>
+                                </p>
+                            </li>
+                            <li>
+                            <img src="" className="contact-icon" alt="LinkedIn icon"/>
+                                <p className="contact-header">LinkedIn</p>
+                                <p className="contact-info"><a href="https://www.linkedin.com/in/joanna-pompeo">Connect</a></p>
+                            </li>
+                            <li>
+                            <img src="" className="contact-icon" alt="GitHub icon"/>
+                                <p className="contact-header">GitHub</p>
+                                <p className="contact-info">
+                                    <a href="https://github.com/jpompeo">Check out my Code</a>
+                                </p>
+                            </li>
+                            </ul>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
