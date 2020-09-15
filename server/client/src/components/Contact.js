374a32899951518b260a6be2585b6e822bad4929
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-09-15 02:06:22 -0400

    Adding links and icons to contacts page

--- server/client/src/components/Contact.js
+++ server/client/src/components/Contact.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Row, Col, Container } from "react-bootstrap";
 import '../styles/Contact.css';
+import { BsEnvelopeFill } from 'react-icons/bs';
+import { MdPhoneAndroid } from 'react-icons/md';
+import { FaLinkedin } from 'react-icons/fa';
+import { SiGithub } from 'react-icons/si';
+
 
 const Contact = () => {
     return (
@@ -17,44 +22,61 @@ const Contact = () => {
             </Row>
 
             <Row>
-                {/* <Col sm={6} md={5} lg={5}>
-                     Headshot 
-                    <div id="headshot-container">
-                        <img id="contact-image" src="" alt="" />
-                    </div>
-                </Col> */}
-
                 <Col>
                     {/* Bio */}
-                        <div id="contact-list-container">
-                            <ul id="contact-list">
-                            <li>
-                                <img src="" className="contact-icon" alt="Email icon"/>
-                                <p className="contact-header">Email</p>
-                                <p className="contact-info">
-                                    <a href="mailto:joannapompeo@gmail.com">joannapompeo@gmail.com</a>
-                                </p>
-                            </li>
-                            <li>
-                            <img src="" className="contact-icon" alt="Phone icon"/>
-                                <p className="contact-header">Phone</p>
-                                <p className="contact-info">
-                                    <a href="tel:561-236-1615">561-236-1615</a>
-                                </p>
-                            </li>
-                            <li>
-                            <img src="" className="contact-icon" alt="LinkedIn icon"/>
-                                <p className="contact-header">LinkedIn</p>
-                                <p className="contact-info"><a href="https://www.linkedin.com/in/joanna-pompeo">Connect</a></p>
-                            </li>
-                            <li>
-                            <img src="" className="contact-icon" alt="GitHub icon"/>
-                                <p className="contact-header">GitHub</p>
-                                <p className="contact-info">
-                                    <a href="https://github.com/jpompeo">Check out my Code</a>
-                                </p>
-                            </li>
-                            </ul>
+                    <div id="contact-list-container">
+                        <div id="contact-list">
+                            <a href="mailto:joannapompeo@gmail.com">
+                                <div>
+                                    {/* <img src="" className="contact-icon" alt="Email icon" /> */}
+                                        <p className="contact-header">Email</p>
+                                    <div className="contact-icon">
+                                        <BsEnvelopeFill
+                                            size="5em"
+                                            // color="rgba(27, 36, 47, .6)"
+                                        />
+                                    </div>
+                                    <p className="contact-info">joannapompeo@gmail.com</p>
+                                </div>
+                            </a>
+                            <a href="tel:561-236-1615">
+                                <div>
+                                    <p className="contact-header">Phone</p>
+                                    <div className="contact-icon">
+                                        <MdPhoneAndroid
+                                            size="5em"
+                                            // color="rgba(27, 36, 47, .6)"
+                                        />
+                                    </div>
+                                    <p className="contact-info">561-236-1615</p>
+                                </div>
+                            </a>
+                            <a href="https://www.linkedin.com/in/joanna-pompeo" target="_blank">
+                                <div>
+                                    <p className="contact-header">LinkedIn</p>
+                                    <div className="contact-icon">
+                                        <FaLinkedin
+                                            size="5em"
+                                            // color="rgba(27, 36, 47, .6)"
+                                        />
+                                    </div>
+                                    <p className="contact-info">Connect</p>
+                                </div>
+                            </a>
+                            <a href="https://github.com/jpompeo" target="_blank">
+                                <div>
+                                    <p className="contact-header">GitHub</p>
+                                    <div className="contact-icon">
+                                        <SiGithub
+                                            className="contact-icon2"
+                                            size="5em"
+                                            // color="rgba(27, 36, 47, .6)"
+                                        />
+                                    </div>
+                                    <p className="contact-info">Check out my code</p>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </Col>
             </Row>
