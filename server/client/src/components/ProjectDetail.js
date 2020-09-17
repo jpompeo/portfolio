b8dfccdc85430e3d8f1fe7a75d43bcea67700141
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-09-17 18:25:11 -0400

    Connecting overdrive to projects for transitions

--- server/client/src/components/ProjectDetail.js
+++ server/client/src/components/ProjectDetail.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Link } from 'react-router-dom';
+import { Row, Col, Container, Button } from "react-bootstrap";
+import '../styles/ProjectDetail.css';
+import { BsArrowLeft } from 'react-icons/bs';
+import Overdrive from 'react-overdrive';
 
-const ProjectDetail = () => {
+const Bio = () => {
+
+    const renderProject = () => {
+        return (
+            <Overdrive id="project-transition">
+                <div id="project-detail">
+
+                </div>
+            </Overdrive>
+        )
+    }
+
     return (
-        <h1>Project Details</h1>
+        <Container id="project" fluid>
+            <Row>
+                <Col>
+                    <header id="project-title">
+                        <h1>Projects</h1>
+                    </header>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <div id="project-detail-container">
+                        {/* return to projects page */}
+                        <div className="back-link-container">
+                            <Link to="/projects">
+                                <button className="back-button">
+                                    <BsArrowLeft /> Back
+                                </button>
+                            </Link>
+                        </div>
+
+                        {/* Project Details  */}
+                        {renderProject()}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
-export default ProjectDetail;
+export default Bio;
--- server/client/src/components/ProjectList.js
+++ server/client/src/components/ProjectList.js
@@ -7,14 +7,12 @@ import { BsBoxArrowUpRight } from 'react-icons/bs';
 import { FiArrowUpRight } from 'react-icons/fi';
 import { RiArrowRightUpLine } from 'react-icons/ri';
 import { CgArrowTopRightR } from 'react-icons/cg';
+import Overdrive from 'react-overdrive';
 
-class ProjectList extends Component {
-    constructor(props) {
-        super();
-    }
+const ProjectList = () => {
 
-    renderProjects = () => {
-        console.log(projectInfo);
+
+    const renderProjects = () => {
 
         return projectInfo.map((project, index) => {
             return (
@@ -24,13 +22,19 @@ class ProjectList extends Component {
                         <p className="project-list-type">{project.type}</p>
                         <img className="project-thumbnail" src={project.imageMain} alt="Screenshot of project" />
                         <div className="project-list-title-container">
-                        <h2 className="project-list-title">{project.title}</h2>
+                            <h2 className="project-list-title">{project.title}</h2>
                         </div>
                         <Link to="/projects/write-track">
+                    <Overdrive 
+                        id="project-transition"
+                        animationDelay={1}
+                        // easing={1}
+                    >
                             <button type="button" className="view-project-button">
-                                Learn More 
-                                <span className="hide"> <BsBoxArrowUpRight/></span>
+                                Learn More
+                                <span className="hide"> <BsBoxArrowUpRight /></span>
                             </button>
+                    </Overdrive>
                         </Link>
                     </div>
                 </Col>
@@ -38,31 +42,29 @@ class ProjectList extends Component {
         })
     }
 
-    render() {
-        return (
-            <Container id="projects" fluid>
-                <Row>
-                    <Col>
-                        <header id="projects-title">
-                            <h1>Projects</h1>
-                        </header>
+    return (
+        <Container id="projects" fluid>
+            <Row>
+                <Col>
+                    <header id="projects-title">
+                        <h1>Projects</h1>
+                    </header>
 
-                    </Col>
-                </Row>
+                </Col>
+            </Row>
 
-                <Row>
-                    <Col>
-                        <Container fluid id="projects-list-container">
-                            <Row>
-                                {/* Projects (list/summary view)*/}
-                                {this.renderProjects()}
-                            </Row>
-                        </Container>
-                    </Col>
-                </Row>
-            </Container >
-        )
-    }
+            <Row>
+                <Col>
+                    <Container fluid id="projects-list-container">
+                        <Row>
+                            {/* Projects (list/summary view)*/}
+                            {renderProjects()}
+                        </Row>
+                    </Container>
+                </Col>
+            </Row>
+        </Container >
+    )
 }
 
 export default ProjectList;
