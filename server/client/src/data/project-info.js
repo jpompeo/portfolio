e2f3d9c9aaa34554d01b5444e0eda0fa58c87109
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-09-16 23:55:31 -0400

    Adding data for projects

--- server/client/src/data/project-info.js
+++ server/client/src/data/project-info.js
@@ -1,54 +1,77 @@
-export const projectInfo = 
-    {
-        project1: {
+export const projectInfo = [
+        {
+            id: 'project1',
             title: 'Write Track',
-            type: 'Full Stack',
-            summary: 'summary wt',
-            tech: 'tech wt',
-            imageUrl: '',
-            link: 'http://write-track.herokuapp.com'
+            stack: 'Full Stack',
+            type: 'Individual',
+            summary: 'Designed for authors intending to write a book, this application allows users to set goals, track updates, plan out chapters, and visualize their progress with a graphic dashboard and chart analytics.',
+            tech: 'Developed with React, Redux, Node.js, Express, utilizing MongoDB for data storage and Chart.js for data visualization',
+            imageMain: '',
+            imageCode: '',
+            linkHeroku: 'http://write-track.herokuapp.com',
+            linkGithub: 'https://github.com/jpompeo/writing-app'
         },
-        project2: {
+        {
+            id: 'project2',
             title: 'Muze Networking',
-            type: 'Full Stack / Collaborative',
-            summary: 'summary muze',
-            tech: 'tech muze',
-            imageUrl: '',
-            link: ''
+            stack: 'Full Stack',
+            type: 'Collaborative',
+            summary: 'This real-time chat application is intended for hosting virtual events in which users can participate in multiple conversations simultaneously, create new chats on custom topics, and engage in virtual networking.',
+            tech: 'Utilized Socket.IO with React, Redux, Node.js, Express, and MongoDB, working with nine other software engineers in an Agile environment.',
+            imageMain: '',
+            imageCode: '',
+            linkHeroku: 'http://muze-network.herokuapp.com',
+            linkGithub: 'https://github.com/spaldingVance/Virtual-Networking'
         },
-        project3: {
-            title: 'Project List',
-            type: 'Full Stack',
-            summary: 'pl summary',
-            tech: 'pl tech',
-            imageUrl: '',
-            link: ''
+        {
+            id: 'project3',
+            title: 'Eccentric Essentials',
+            stack: 'Full Stack',
+            type: 'Individual',
+            summary: 'This Full Stack shopping app allows users to search products by keyword, filter by category, and sort by price, utilizing pagination and a REST API to efficiently send data from the server to the client.',
+            tech: 'Utilizing Redux for state management, this app uses React on the Front End and a RESTful API built with Node.js and Express on the Back End.',
+            imageMain: '',
+            imageCode: '',
+            linkHeroku: '',
+            linkGithub: 'https://github.com/jpompeo/product-list'
         },
-        project4: {
-            title: 'Sunglasses',
-            type: 'Back End',
-            summary: 'sg summary',
-            tech: 'sg tech',
-            imageUrl: '',
-            link: ''
+        {
+            id: 'project4',
+            title: 'Sunglasses.io',
+            stack: 'Back End',
+            type: 'Individual',
+            summary: 'Practicing Test Driven Development and proper documentation of code, this Back End application was created to support a mock sunglasses storefront, utilizing a RESTful API and user authentication.',
+            tech: 'Server-side application built with Node.js, producing 60+ tests with Mocha and Chai, and documenting the REST API with Swagger',
+            imageMain: '',
+            imageCode: '',
+            linkHeroku: '',
+            linkGithub: 'https://github.com/jpompeo/sunglasses-io'
         },
-        project5: {
+        {
+            id: 'project5',
             title: 'Bespoke Books',
-            type: 'Front End / Collaborative',
-            summary: 'bb summary',
-            tech: 'bb tech',
-            imageUrl: '',
-            link: ''
+            stack: 'Front End',
+            type: 'Collaborative',
+            summary: 'Intended as a curated collection of book titles, this app allows users to find book suggestions by theme and add recommendations without arbitrary star ratings, allowing for a more meaningful user experience.',
+            tech: 'Partnering with 1 other Software Engineer in a 2-day Hackathon, this app was built on React, utilizing Redux for state management and Google Books API for data.',
+            imageMain: '',
+            imageCode: '',
+            linkHeroku: '',
+            linkGithub: 'https://github.com/jpompeo/bespoke-books'
         },
-        project6: {
-            title: 'Contact List',
-            type: 'Front End',
-            summary: 'cl summary',
-            tech: 'cl tech',
-            imageUrl: '',
-            link: ''
+        {
+            id: 'project6',
+            title: 'Breakfast Club Contacts',
+            stack: 'Front End',
+            type: 'Individual',
+            summary: 'Intended as a modern-day Rolodex, this application allows a user to organize their contact list, with the ability to add, remove, edit, or view details of all personal contacts.',
+            tech: 'Created with React, this Front End application utilizes React Router for navigation and CSS3 with Bootstrap for styling.',
+            imageMain: '',
+            imageCode: '',
+            linkHeroku: '',
+            linkGithub: 'https://github.com/jpompeo/contact-list'
         }
-    }
+]
 
 
 
